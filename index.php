ae5d945945ef3043f7cde930c8d3373d5f77a3b1
andreluiskunert <pesquisador036@gmail.com>
2025-04-09 15:53:28 -0300

    SegundaAlteração

--- index.php
+++ index.php
@@ -7,5 +7,25 @@
 </head>
 <body>
 <h1> Aqui será salvo os arquivos de curso Bootcamp de SQL: Scripts, MySQL e Modelagem Dados pela Udemy </h1>
+<p>
+Bem-vindo(a)
+
+
+----------------------------------------------
+
+
+PRIMEIRO PASSO: Antes de começar esse curso, increva-se no nosso YouTube para ter acesso a diversas aulas gratuitas. Inscrevendo-se, você estará apoiando a produção conteúdos gratuitos de qualidade :)
+
+CLIQUE AQUI => https://www.youtube.com/fva-edu
+
+----------------------------------------------
+
+
+Seja muito bem-vindo(a) a esta jornada inspiradora do Intensivão de SQL! Aqui, você não apenas aprenderá os segredos dessa linguagem poderosa, mas também descobrirá o incrível potencial que possui para moldar seu próprio sucesso. Cada desafio superado será um degrau a mais em direção à sua realização. Vamos juntos transformar conhecimento em conquistas extraordinárias!
+
+Agora, vamos mergulhar nos benefícios do Intensivão de SQL, o curso que impulsionará sua jornada de desenvolvimento profissional. Este curso abrangente e gratuito, dividido em três módulos, proporcionará a você uma compreensão sólida do mundo do SQL:
+
+Módulo 1 - Explorando o Universo SQL: Aqui, você construirá a base, criando um Modelo de Entidade e Relacionamento (MER) do zero, utilizando o MySQL Workbench.
+</p>
 </body>
 </html>
